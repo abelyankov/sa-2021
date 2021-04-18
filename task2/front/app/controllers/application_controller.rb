baa5ec9578ed72fcb6d2c76cf74ac790159f51ac
abelyankov <arthurbelyankove@gmail.com>
2021-04-18 14:55:42 +0600

    minor

--- task2/front/app/controllers/application_controller.rb
+++ task2/front/app/controllers/application_controller.rb
@@ -10,23 +10,23 @@ class ApplicationController < ActionController::Base
   end
 
   def students
-    response = get_http
+    response = get_http("students")
     @response = JSON.parse(response.body)
   end
 
   def works
-    response = get_http
+    response = get_http("works")
     @response = JSON.parse(response.body)
   end
 
-  private def get_http
+  private def get_http(path)
     require 'net/http'
     require 'json'
   
     @host = '127.0.0.1'
     @port = '10001'
   
-    @path = "/students"
+    @path = path
     
     request = Net::HTTP::Get.new(@path, initheader = {'Content-Type' =>'application/json'})
     response = Net::HTTP.new(@host, @port).start {|http| http.request(request) }
--- task2/front/config/application.rb
+++ task2/front/config/application.rb
@@ -23,7 +23,8 @@ module Front
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    config.hosts << "127.0.0.1:10001"
+    config.web_console.whitelisted_ips = '172.17.0.1'
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
